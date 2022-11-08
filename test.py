d09cbb50a5c8b890261496485935f471f81154b6
j-brown321 <jbrown4072@gmail.com>
2022-11-08 00:19:44 -0600

    booyah

--- test.py
+++ test.py
@@ -1,1 +1,23 @@
-blah blah blah 
+with open('testdoc.txt') as line:
+
+    charac = line.read()
+
+    print(charac)
+
+def wordfreq(string):
+
+    list = string.split()
+    
+    newlist =  []
+
+    for x in list :
+        
+        if x not in newlist :
+            
+            newlist.append(x)
+    
+    for x in range (0, len(newlist)) :
+        
+        print("The frequncy of the word", newlist[x], "is", list.count(newlist[x]))
+
+        wordfreq(charac)
