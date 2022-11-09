7c1685c76eabb661cbaa6c919d3049762e7ad189
j-brown321 <jbrown4072@gmail.com>
2022-11-08 18:08:47 -0600

    on the feature branch

--- test.py
+++ test.py
@@ -21,3 +21,26 @@ def wordfreq(string):
         print("The frequncy of the word", newlist[x], "is", list.count(newlist[x]))
 
         wordfreq(charac)
+
+import os
+import sys
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pim.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
+
+
